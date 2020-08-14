aae7a1b19c4456464c1d9ffe6552b253d9d62702
ruanhang <1340215221@qq.com>
2020-08-14 13:26:56 +0800

    update

--- src/main/java/com/rh/note/NoteApplication.java
+++ src/main/java/com/rh/note/NoteApplication.java
@@ -6,13 +6,13 @@ import com.rh.note.config.BeanConfig;
 /**
  * 笔记软件启动程序
  */
-public class NoteApplication implements BeanConfig {
+public class NoteApplication {
 
     public static void main(String[] args) {
         System.out.println("启动笔记软件");
         FlatDarculaLaf.install();
         System.out.println("完成Darcula主题初始化");
-        projectListAction.startFrame();
+        BeanConfig.projectListAction.startFrame();
         System.out.println("完成[笔记APP]初始化");
     }
 
