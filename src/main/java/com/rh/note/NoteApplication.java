a3d022cea10135351ea469e1302097b3d8837e7c
ruanhang <1340215221@qq.com>
2020-08-17 14:50:16 +0800

    配置日志

--- src/main/java/com/rh/note/NoteApplication.java
+++ src/main/java/com/rh/note/NoteApplication.java
@@ -2,18 +2,22 @@ package com.rh.note;
 
 import com.formdev.flatlaf.FlatDarculaLaf;
 import com.rh.note.config.BeanConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 笔记软件启动程序
  */
 public class NoteApplication {
 
+    private static Logger log = LoggerFactory.getLogger(NoteApplication.class);
+
     public static void main(String[] args) {
-        System.out.println("启动笔记软件");
+        log.info("启动笔记软件");
         FlatDarculaLaf.install();
-        System.out.println("完成Darcula主题初始化");
+        log.info("完成Darcula主题初始化");
         BeanConfig.projectListAction.startFrame();
-        System.out.println("完成[笔记APP]初始化");
+        log.info("完成[笔记APP]初始化");
     }
 
 }
