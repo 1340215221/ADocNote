a6901824323a0c0dd58a5233af9de3de2e811e5d
ruanhang <1340215221@qq.com>
2020-11-10 17:35:27 +0800

    update

--- j-note/src/main/java/com/rh/note/constants/BaseConstants.java
+++ j-note/src/main/java/com/rh/note/constants/BaseConstants.java
@@ -16,7 +16,7 @@ public interface BaseConstants {
     /**
      * 文件名正则
      */
-    String file_name_regex = "[0-9a-zA-Z\\u4e00-\\u9fa5_,\\.\\-\\s]+";
+    String file_name_regex = "[0-9a-zA-Z\\u4e00-\\u9fa5_,\\.\\-\\s+<>]+";
     /**
      * 文件路径正则
      */
