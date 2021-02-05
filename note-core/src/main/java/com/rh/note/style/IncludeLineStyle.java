55716280d9f2aee69f42e08c40a92f9d1c428dd2
ruanhang <1340215221@qq.com>
2021-02-05 13:19:42 +0800

    update

--- note-core/src/main/java/com/rh/note/style/IncludeLineStyle.java
+++ note-core/src/main/java/com/rh/note/style/IncludeLineStyle.java
@@ -6,11 +6,21 @@ import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import java.awt.Color;
+
 /**
  * include语句样式
  */
 @RequiredArgsConstructor
 public class IncludeLineStyle {
+
+    /**
+     * include::
+     * []
+     */
+    private static final Color color = Color.decode("#00080");
 
     @NonNull
     private IncludeSyntax syntax;
@@ -28,7 +38,12 @@ public class IncludeLineStyle {
      * 引用头样式
      * include::
      */
-    private @NotNull StyleItem getHeadStyle() {
+    private @Nullable StyleItem getHeadStyle() {
+        SimpleAttributeSet set = new SimpleAttributeSet();
+        StyleConstants.setForeground(set, color);
+        int startOffset = syntax.getIndent().length();
+        int endOffset = startOffset + 10;
+        return StyleItem.getInstance(set, startOffset, endOffset);
     }
 
     /**
@@ -36,6 +51,14 @@ public class IncludeLineStyle {
      * []
      */
     private @Nullable StyleItem getRangeStyle() {
+        if (syntax.getLineNum1() != null || syntax.getLineNum2() != null) {
+            return null;
+        }
+        SimpleAttributeSet set = new SimpleAttributeSet();
+        // 缩进 include:: 路径
+        int startOffset = syntax.getIndent().length() + 9 + syntax.getIncludePath().length();
+        int endOffset = startOffset + 3;
+        return StyleItem.getInstance(set, startOffset, endOffset);
     }
 
     /**
@@ -43,6 +66,14 @@ public class IncludeLineStyle {
      * [lines=1]中的 [
      */
     private @Nullable StyleItem getRangeStylePre() {
+        if (syntax.getLineNum1() == null && syntax.getLineNum2() == null) {
+            return null;
+        }
+        SimpleAttributeSet set = new SimpleAttributeSet();
+        // 缩进 include:: 路径
+        int startOffset = syntax.getIndent().length() + 9 + syntax.getIncludePath().length();
+        int endOffset = startOffset + 1;
+        return StyleItem.getInstance(set, startOffset, endOffset);
     }
 
     /**
@@ -50,6 +81,21 @@ public class IncludeLineStyle {
      * [lines=1]中的 ]
      */
     private @Nullable StyleItem getRangeStyleSub() {
+        if (syntax.getLineNum1() == null && syntax.getLineNum2() == null) {
+            return null;
+        }
+        SimpleAttributeSet set = new SimpleAttributeSet();
+        int startOffset;
+        // 缩进 include:: 路径 [lines=1..2
+        if (syntax.getLineNum1() != null && syntax.getLineNum2() != null) {
+            startOffset = syntax.getIndent().length() + 9 + syntax.getIncludePath().length() +;
+        // 缩进 include:: 路径 [lines=1
+        } else {
+            startOffset = syntax.getIndent().length() + 9 + syntax.getIncludePath().length() +;
+        }
+        int startOffset = syntax.getIndent().length() + 9 + syntax.getIncludePath().length() + ;
+        int endOffset = startOffset + 1;
+        return StyleItem.getInstance(set, startOffset, endOffset);
     }
 
 }
